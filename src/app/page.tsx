7bfbf3456be804ce97d32bdf77a60d27ab017148
Beatriz Medeiros <beatrizml90@hotmail.com>
2025-02-19 22:02:35 -0300

    Aula 1: Finalizado componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Copy} from "lucide-react";
+import { ArrowRight, Copy, Mail } from "lucide-react";
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
@@ -13,6 +14,14 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
